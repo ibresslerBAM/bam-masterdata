ede2c8ea15ab50e61d7516dcb2874fc599d3f486
Jose M. Pizarro <112697669+JosePizarro3@users.noreply.github.com>
2024-12-18 09:55:06 +0100

    Create datamodel base classes based on openbisschema (#10)
    * Added definitions for object types, vocabulary types, and property types
    
    Defined abstract class for definitions
    
    Define abstract BaseObjectTypeDef for shared object, collection and dataset types
    
    Created DataType enums
    
    Created BaseEntity and abstract classes for object types and vocabulary types
    
    Added properties and terms list for to_json method
    
    * Added examples for datamodel: object types and vocabulary types
    
    * Added testing for metadata
    
    * Added tests for entities
    
    Fixed tests and classes
    
    * Added empty classes for collection and property types
    
    * Added description for metadata in PropertyTypeDef
    
    Deleted minor question
    
    Added actions on save in settings.json
    
    * Fix testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
 import os
 
 import pytest
+from pydantic import ConfigDict
+
+from bam_data_store.metadata.definitions import (
+    ObjectTypeDef,
+    PropertyTypeAssignment,
+    VocabularyTerm,
+    VocabularyTypeDef,
+)
+from bam_data_store.metadata.entities import BaseEntity, ObjectType, VocabularyType
 
 if os.getenv('_PYTEST_RAISE', '0') != '0':
 
@@ -11,3 +20,129 @@ if os.getenv('_PYTEST_RAISE', '0') != '0':
     @pytest.hookimpl(tryfirst=True)
     def pytest_internalerror(excinfo):
         raise excinfo.value
+
+
+class MockedEntity(BaseEntity):
+    model_config = ConfigDict(ignored_types=(ObjectTypeDef, PropertyTypeAssignment))
+    defs = ObjectTypeDef(
+        version=1,
+        code='MOCKED_ENTITY',
+        description="""
+        Mockup for an entity definition//Mockup für eine Entitätsdefinition
+        """,
+        generated_code_prefix='MOCKENT',
+    )
+
+    name = PropertyTypeAssignment(
+        version=1,
+        code='$NAME',
+        data_type='VARCHAR',
+        property_label='Name',
+        description="""
+        Name
+        """,
+        mandatory=True,
+        show_in_edit_views=True,
+        section='General information',
+    )
+
+
+class MockedObjectType(ObjectType):
+    defs = ObjectTypeDef(
+        version=1,
+        code='MOCKED_OBJECT_TYPE',
+        description="""
+        Mockup for an object type definition
+        """,
+        generated_code_prefix='MOCKOBJTYPE',
+    )
+
+    name = PropertyTypeAssignment(
+        version=1,
+        code='$NAME',
+        data_type='VARCHAR',
+        property_label='Name',
+        description="""
+        Name
+        """,
+        mandatory=True,
+        show_in_edit_views=True,
+        section='General information',
+    )
+
+    alias = PropertyTypeAssignment(
+        version=1,
+        code='ALIAS',
+        data_type='VARCHAR',
+        property_label='Alias',
+        description="""
+        Alias
+        """,
+        mandatory=False,
+        show_in_edit_views=True,
+        section='General information',
+    )
+
+
+class MockedObjectTypeLonger(MockedObjectType):
+    defs = ObjectTypeDef(
+        version=1,
+        code='MOCKED_OBJECT_TYPE_LONGER',
+        description="""
+        Mockup for an object type definition with more property type assignments
+        """,
+        generated_code_prefix='MOCKOBJTYPELONG',
+    )
+
+    settings = PropertyTypeAssignment(
+        version=1,
+        code='SETTINGS',
+        data_type='MULTILINE_VARCHAR',
+        property_label='Settings',
+        description="""
+        Settings
+        """,
+        mandatory=False,
+        show_in_edit_views=True,
+        section='General information',
+    )
+
+
+class MockedVocabularyType(VocabularyType):
+    defs = VocabularyTypeDef(
+        version=1,
+        code='MOCKED_VOCABULARY_TYPE',
+        description="""
+        Mockup for an vocabulary type definition
+        """,
+    )
+
+    option_a = VocabularyTerm(
+        version=1,
+        code='OPTION_A',
+        label='Option A',
+        description='Option A from two possible options in the vocabulary',
+    )
+
+    option_b = VocabularyTerm(
+        version=1,
+        code='OPTION_B',
+        label='Option B',
+        description='Option B from two possible options in the vocabulary',
+    )
+
+
+def generate_base_entity():
+    return MockedEntity()
+
+
+def generate_object_type():
+    return MockedObjectType()
+
+
+def generate_object_type_longer():
+    return MockedObjectTypeLonger()
+
+
+def generate_vocabulary_type():
+    return MockedVocabularyType()
