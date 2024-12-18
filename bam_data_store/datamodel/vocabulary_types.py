01310f146b9363262cb9c4245a96573e6b7eb01f
Jose M. Pizarro <112697669+JosePizarro3@users.noreply.github.com>
2024-12-18 09:55:06 +0100

    Create datamodel base classes based on openbisschema (#10)
    * Added definitions for object types, vocabulary types, and property types
    
    Defined abstract class for definitions
    
    Define abstract BaseObjectTypeDef for shared object, collection and dataset types
    
    Created DataType enums
    
    Created BaseEntity and abstract classes for object types and vocabulary types
    
    Added properties and terms list for to_json method
    
    * Added examples for datamodel: object types and vocabulary types
    
    * Added testing for metadata
    
    * Added tests for entities
    
    Fixed tests and classes
    
    * Added empty classes for collection and property types
    
    * Added description for metadata in PropertyTypeDef
    
    Deleted minor question
    
    Added actions on save in settings.json
    
    * Fix testing

---
+++ bam_data_store/datamodel/vocabulary_types.py
@@ -0,0 +1,83 @@
+from bam_data_store.metadata.definitions import (
+    VocabularyTerm,
+    VocabularyTypeDef,
+)
+from bam_data_store.metadata.entities import VocabularyType
+
+
+class DocumentType(VocabularyType):
+    defs = VocabularyTypeDef(
+        version=1,
+        code='DOCUMENT_TYPE',
+        description='Document type//Dokumententypen',
+    )
+
+    acceptance_certificate = VocabularyTerm(
+        version=1,
+        code='ACCEPTANCE_CERTIFICATE',
+        label='Acceptance Certificate',
+        description='Acceptance Certificate//Abnahmezeugnis',
+    )
+
+    calibration_certificate = VocabularyTerm(
+        version=1,
+        code='CALIBRATION_CERTIFICATE',
+        label='Calibration Certificate',
+        description='Calibration Certificate//Kalibrierschein',
+    )
+
+    catalog = VocabularyTerm(
+        version=1,
+        code='CATALOG',
+        label='Catalog',
+        description='Document type//Dokumententypen',
+    )
+
+    datasheet = VocabularyTerm(
+        version=1,
+        code='DATASHEET',
+        label='Datasheet',
+        description='Datasheet//Datenblatt',
+    )
+
+    datasheet_msds = VocabularyTerm(
+        version=1,
+        code='DATASHEET_MSDS',
+        label='Material Safety Datasheet',
+        description='Material Safety Datasheet (MSDS)//Sicherheitsdatenblatt',
+    )
+
+    datasheet_technical = VocabularyTerm(
+        version=1,
+        code='DATASHEET_TECHNICAL',
+        label='Technical Datasheet',
+        description='Technical Datasheet//Technisches Datenblatt',
+    )
+
+    delivery_note = VocabularyTerm(
+        version=1,
+        code='DELIVERY_NOTE',
+        label='Delivery Note',
+        description='Delivery Note//Lieferschein',
+    )
+
+    diagram = VocabularyTerm(
+        version=1,
+        code='DIAGRAM',
+        label='Diagram',
+        description='Diagram//Diagramm',
+    )
+
+    fabrication_order = VocabularyTerm(
+        version=1,
+        code='FABRICATION_ORDER',
+        label='Fabrication Order',
+        description='Fabrication Order//Fertigungsauftrag',
+    )
+
+    inspection_certificate = VocabularyTerm(
+        version=1,
+        code='INSPECTION_CERTIFICATE',
+        label='Inspection Certificate',
+        description='Inspection Certificate//Inspektionszeugnis',
+    )
---
+++ bam_data_store/metadata/entities.py
@@ -0,0 +1,137 @@
+import json
+from typing import Any, Optional
+
+from pydantic import BaseModel, ConfigDict, Field, model_validator
+
+from bam_data_store.metadata.definitions import (
+    ObjectTypeDef,
+    PropertyTypeAssignment,
+    VocabularyTerm,
+    VocabularyTypeDef,
+)
+
+
+class BaseEntity(BaseModel):
+    """
+    Base class used to define `ObjectType` and `VocabularyType` classes. It extends the `BaseModel`
+    adding new methods that are useful for interfacing with openBIS.
+    """
+
+    def to_json(self, indent: Optional[int] = None) -> str:
+        """
+        Returns the model as a string in JSON format storing the data `defs` and the property or
+        vocabulary term assignments.
+
+        Args:
+            indent (Optional[int], optional): The indent to print in JSON. Defaults to None.
+
+        Returns:
+            str: The JSON representation of the model.
+        """
+        # * `model_dump_json()` from pydantic does not store the `defs` section of each entity.
+        data = self.model_dump()
+
+        attr_value = getattr(self, 'defs')
+        if isinstance(attr_value, BaseModel):
+            data['defs'] = attr_value.model_dump()
+        else:
+            data['defs'] = attr_value
+
+        return json.dumps(data, indent=indent)
+
+    def to_dict(self) -> dict:
+        """
+        Returns the model as a dictionary storing the data `defs` and the property or vocabulary term
+        assignments.
+
+        Returns:
+            dict: The dictionary representation of the model.
+        """
+        dump_json = self.to_json()
+        return json.loads(dump_json)
+
+
+class ObjectType(BaseEntity):
+    """
+    Base class used to define object types. All object types must inherit from this class. The
+    object types are defined in the module `bam_data_store/object_types.py`.
+
+    The `ObjectType` class contains a list of all `properties` defined for a `ObjectType`, for
+    internally represent the model in other formats (e.g., JSON or Excel).
+    """
+
+    model_config = ConfigDict(ignored_types=(ObjectTypeDef, PropertyTypeAssignment))
+
+    properties: list[PropertyTypeAssignment] = Field(
+        default=[],
+        description="""
+        List of properties assigned to an object type. This is useful for internal representation of the model.
+        """,
+    )
+
+    @model_validator(mode='after')
+    @classmethod
+    def model_validator_after_init(cls, data: Any) -> Any:
+        """
+        Validate the model after instantiation of the class.
+
+        Args:
+            data (Any): The data containing the fields values to validate.
+
+        Returns:
+            Any: The data with the validated fields.
+        """
+        # Add all the properties assigned to the object type to the `properties` list.
+        for attr_name in dir(cls):
+            attr = getattr(cls, attr_name)
+            if isinstance(attr, PropertyTypeAssignment):
+                data.properties.append(attr)
+
+        return data
+
+
+class VocabularyType(BaseEntity):
+    """
+    Base class used to define vocabulary types. All vocabulary types must inherit from this class. The
+    vocabulary types are defined in the module `bam_data_store/vocabulary_types.py`.
+
+    The `VocabularyType` class contains a list of all `terms` defined for a `VocabularyType`, for
+    internally represent the model in other formats (e.g., JSON or Excel).
+    """
+
+    model_config = ConfigDict(ignored_types=(VocabularyTypeDef, VocabularyTerm))
+
+    terms: list[VocabularyTerm] = Field(
+        default=[],
+        description="""
+        List of vocabulary terms. This is useful for internal representation of the model.
+        """,
+    )
+
+    @model_validator(mode='after')
+    @classmethod
+    def model_validator_after_init(cls, data: Any) -> Any:
+        """
+        Validate the model after instantiation of the class.
+
+        Args:
+            data (Any): The data containing the fields values to validate.
+
+        Returns:
+            Any: The data with the validated fields.
+        """
+        # Add all the vocabulary terms defined in the vocabulary type to the `terms` list.
+        for attr_name in dir(cls):
+            attr = getattr(cls, attr_name)
+            if isinstance(attr, VocabularyTerm):
+                data.terms.append(attr)
+
+        return data
+
+
+class PropertyType(BaseEntity):
+    pass
+
+
+class CollectionType(BaseEntity):
+    pass
