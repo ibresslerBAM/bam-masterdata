a4a8c3974b25966133d0f47a0f9f4aa9798f71b7
jpizarro <jose.pizarro-blanco@bam.de>
2025-01-29 13:24:26 +0100

    Clean up script for installation and pyproject

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -3,9 +3,6 @@
 # Fail immediately if any command exits with a non-zero status
 set -e
 
-echo "Initializing submodules..."
-git submodule update --init --recursive
-
 echo "Making sure pip is up to date..."
 pip install --upgrade pip
 
@@ -13,18 +10,4 @@ echo "Installing uv..."
 pip install uv
 
 echo "Installing main project dependencies..."
-uv pip install -e '.[dev,docu,jupy]'
-
-echo "Installing submodule dependencies..."
-SUBMODULE_PATH="bam_masterdata/dependencies/openbisschema"
-
-if [ -d "$SUBMODULE_PATH" ]; then
-    pushd "$SUBMODULE_PATH" > /dev/null
-    uv pip install -e '.[test,doc]'
-    popd > /dev/null
-else
-    echo "Submodule path $SUBMODULE_PATH does not exist. Initialization may have failed."
-    exit 1
-fi
-
-echo "All dependencies installed successfully."
+uv pip install -e '.[dev,docu]'
