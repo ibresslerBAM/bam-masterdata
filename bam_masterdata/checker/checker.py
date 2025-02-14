1605739a500be9b742f4b05de548fa653b3356ce
carlosmada22 <carlosmadariagaaramendi@gmail.com>
2025-02-14 11:40:25 +0100

    Fixed imports

--- bam_masterdata/checker/checker.py
+++ bam_masterdata/checker/checker.py
@@ -1,7 +1,9 @@
-from bam_masterdata.checker import DataModelLoader, MasterDataValidator, SourceLoader
+from bam_masterdata.checker.datamodel_loader import DataModelLoader
+from bam_masterdata.checker.masterdata_validator import MasterDataValidator
+from bam_masterdata.checker.source_loader import SourceLoader
 
 
-class DataModelComparator:
+class MasterdataChecker:
     def __init__(self, validation_rules_path: str):
         """
         Initialize the comparator with validation rules.
@@ -20,15 +22,15 @@ class DataModelComparator:
         """
         Load and transform the current data model (Pydantic classes) into JSON.
         """
-        loader = DataModelLoader(source_path)
-        self.current_model = loader.parse_pydantic_models()
+        # loader = DataModelLoader(source_path)
+        # self.current_model = loader.parse_pydantic_models()
 
     def load_new_entities(self, source: str, source_type: str):
         """
         Load new entities from various sources (Python classes, Excel, etc.).
         """
-        loader = SourceLoader(source, source_type)
-        self.new_entities = loader.load()
+        # loader = SourceLoader(source, source_type)
+        # self.new_entities = loader.load()
 
     def validate(self, mode: str = "all") -> dict:
         """
