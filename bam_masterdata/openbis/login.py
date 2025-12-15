4211fb4aa1b196c5b2de6d45fa85a36baaec84cc
Çağtay Fabry <cagtay.fabry@bam.de>
2025-12-15 08:19:37 +0100

    allow Openbis session in ologin (#209)

--- bam_masterdata/openbis/login.py
+++ bam_masterdata/openbis/login.py
@@ -3,16 +3,23 @@ from pybis import Openbis
 
 
 # Connect to openBIS
-def ologin(url: str = "") -> Openbis:
+def ologin(url: str | Openbis = "") -> Openbis:
     """
     Connect to openBIS using the credentials stored in the environment variables.
 
+    If an existing Openbis session is provided, the session is returned.
+
     Args:
-        url (str): The URL of the openBIS instance. Defaults to the value of the `OPENBIS_URL` environment variable.
+        url (str, Openbis): The URL of the openBIS instance. Defaults to the value of the `OPENBIS_URL` environment variable.
+        An existing Openbis session can be provided.
 
     Returns:
         Openbis: Openbis object for the specific openBIS instance defined in `URL`.
     """
-    o = Openbis(url)
-    o.login(environ("OPENBIS_USERNAME"), environ("OPENBIS_PASSWORD"), save_token=True)
+    if not isinstance(url, Openbis):
+        o = Openbis(url)
+
+    if not o.is_session_activ():
+        o.login(environ("OPENBIS_USERNAME"), environ("OPENBIS_PASSWORD"), save_token=True)
+
     return o
